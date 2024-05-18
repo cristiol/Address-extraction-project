62a82be4d7ec90e8c9f89ebe8b16735e86fc4a29
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-05-18 12:36:47 +0300

    Update main_script.py

--- extraction_algorithm/main_script.py
+++ extraction_algorithm/main_script.py
@@ -33,18 +33,15 @@ def get_address(link, recursion_count=0):
       return None
 
   if geocode_street_addresses and geocode_zipcode_addresses:
-    #print('----')
-    #print('two list root')
+
     final_result = get_final_addresses_by_two_list(geocode_street_addresses, geocode_zipcode_addresses)
     if final_result:
       return append_to_json_file({'link': link.strip('https://'), 'address': final_result})
 
     if final_result is None:
-      #print('---')
-      #print('joined query rout')
+
       if get_final_addresses_by_joined_query(street_address_by_regex, zipcode_addresses_by_regex):
-        #print('---')
-        #print('zipcode route')
+
         final_result = get_final_addresses_by_joined_query(street_address_by_regex, zipcode_addresses_by_regex)
         if final_result:
           return append_to_json_file({'link': link.strip('https://'), 'address': final_result})
@@ -56,16 +53,14 @@ def get_address(link, recursion_count=0):
 
 
   if geocode_street_addresses and geocode_zipcode_addresses is None:
-    #print('----')
-    #print('street regex rout')
+
     if get_final_addresses_by_one_list(geocode_street_addresses):
       final_result = get_final_addresses_by_one_list(geocode_street_addresses)
       if final_result:
         return append_to_json_file({'link': link.strip('https://'), 'address': final_result})
 
   if geocode_street_addresses is None and geocode_zipcode_addresses:
-    #print('---')
-    #print('zipcode route')
+
     if get_final_addresses_by_one_list(geocode_zipcode_addresses):
       final_result = get_final_addresses_by_one_list(geocode_zipcode_addresses)
       if final_result:
@@ -78,7 +73,6 @@ def get_address(link, recursion_count=0):
 # with concurrent.futures.ThreadPoolExecutor() as executor:
 #    results = list(executor.map(get_address, links[250:300]))
 
-for link in links[:3]:
-    get_address(link)
-    print('index: ', links.index(link))
-#print(get_address('https://lrgmarketing.com/'))
+# for link in links[:3]:
+#     get_address(link)
+#     print('index: ', links.index(link))
